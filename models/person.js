bf514bb8d66f7d9fbaeadf11764b20406fb6b261
Paavo <paavo.hintsa@tuni.fi>
2024-11-01 11:52:45 +0200

    Added eslint and fixed stuff according to it

--- models/person.js
+++ models/person.js
@@ -9,30 +9,30 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
 
-  .then(result => {
+  .then(
     console.log('connected to MongoDB')
-  })
+  )
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        //Check format
+        return /^\d{2,3}-\d{5,}$/.test(v)
       },
-    number: {
-        type: String,
-        required: true,
-        validate: {
-          validator: function(v) {
-            //Check format
-            return /^\d{2,3}-\d{5,}$/.test(v);
-          },
-          message: props => `${props.value} is not a valid phone number! It should be in the format XX-XXXXX or XXX-XXXXX and have at least 8 digits in total.`
-        }
-      }
+      message: props => `${props.value} is not a valid phone number! It should be in the format XX-XXXXX or XXX-XXXXX and have at least 8 digits in total.`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
